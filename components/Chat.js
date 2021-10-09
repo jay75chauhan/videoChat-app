04b697aa2a2a7065aa74c8d8d3fcc86528be9f01
jay75chauhan <chauhan75jayk@gmail.com>
2021-10-09 20:55:01 +0530

    youchable opacity

--- components/Chat.js
+++ components/Chat.js
@@ -19,31 +19,33 @@ export default function Chat({ setModalVisible }) {
     <View style={{ flex: 1, backgroundColor: "#1c1c1c" }}>
       <SafeAreaView style={tw`h-full `}>
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <ChatHeader setModalVisible={setModalVisible} />
+          <View style={tw`flex-1`}>
+            <ChatHeader setModalVisible={setModalVisible} />
 
-          <View style={tw`flex-1`}></View>
-          {/* type message */}
-          <View style={tw` py-4 px-2`}>
-            <Text style={tw`text-white ml-3 text-xl`}>Send to: Everyone</Text>
-            <View style={tw`  flex-row items-center justify-between mt-2  `}>
-              <TextInput
-                style={tw`h-12 ml-2 p-2 px-3 text-lg w-10/12 mr-2 rounded-2xl text-white border border-gray-400 `}
-                placeholder="Tap here to chat"
-                placeholderTextColor="#595859"
-                value={messageText}
-                onChangeText={(text) => setMessageText(text)}
-              />
-
-              <TouchableOpacity>
-                <FontAwesome
-                  name={"send"}
-                  size={20}
-                  color="#efefef"
-                  style={tw`p-3 bg-gray-500 ${
-                    messageText && `bg-blue-500`
-                  } rounded-xl`}
+            <View style={tw`flex-1`}></View>
+            {/* type message */}
+            <View style={tw` py-4 px-2`}>
+              <Text style={tw`text-white ml-3 text-xl`}>Send to: Everyone</Text>
+              <View style={tw`  flex-row items-center justify-between mt-2  `}>
+                <TextInput
+                  style={tw`h-12 ml-2 p-2 px-3 text-lg w-10/12 mr-2 rounded-2xl text-white border border-gray-400 `}
+                  placeholder="Tap here to chat"
+                  placeholderTextColor="#595859"
+                  value={messageText}
+                  onChangeText={(text) => setMessageText(text)}
                 />
-              </TouchableOpacity>
+
+                <TouchableOpacity>
+                  <FontAwesome
+                    name={"send"}
+                    size={20}
+                    color="#efefef"
+                    style={tw`p-3 bg-gray-500 ${
+                      messageText && `bg-blue-500`
+                    } rounded-xl`}
+                  />
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
         </TouchableWithoutFeedback>
